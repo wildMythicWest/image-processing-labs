8574e6a98e7d8ea389fa1446d9f4e56e70823772
Boyan Georgiev <gboyan@vmware.com>
2024-10-23 09:54:57 +0300

    Adding some test images

--- lab2.m
+++ lab2.m
@@ -1,7 +1,7 @@
 
 H3 = [
   1 -2 1;
-  -2 5 -2;
+  -2 4 -2;
   1 -2 1;
 ];
 
@@ -15,15 +15,23 @@ H10 = [
   1 2 1;
   0 0 0;
   -1 -2 -1;
-]
+];
 
 H11 = [
   2 1 0;
   1 0 -1;
   0 -1 -2;
-]
+];
 
-analyze_filter(H3, 'H3');
-%analyze_filter(H6, 'H6');
-%analyze_filter(H10, 'H10');
+input_image = imread('coffee.webp');
+% imshow(input_image)
+gray_image = rgb2gray(input_image);
+% imshow(gray_image)
+output_image = filter2(H6, gray_image);
+
+imshow(output_image);
+
+% analyze_filter(H3, 'H3');
+% analyze_filter(H6, 'H6');
+% analyze_filter(H10, 'H10');
 % analyze_filter(H11, 'H11');
